4f2456182993ac1fe0a6d7f29a89e3306aae7094
Jay Landro <jaylandro@gmail.com>
2019-05-18 17:05:13 +0000

    Clean up

---
+++ nuget/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OsnI18n
+{
+    public class countries
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
