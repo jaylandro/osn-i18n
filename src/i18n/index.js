412011bd08e0d054a3c9d6f31c4da1be57087ee4
Jay Landro <jaylandro@gmail.com>
2019-05-18 16:41:32 -0500

    Added Travis CI config as well as update i18n to detect user agent language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,26 +3,22 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
+const languageMappings = {
+  'en': require('./en.json'),
+  'ja': require('./ja/en.json'),
+  'fr': require('./fr/en.json'),
+  'es': require('./es/en.json'),
+  'ar': require('./ar/en.json'),
+  'zh': require('./zh/en.json'),
+};
+const userLanguage = (window.navigator.userLanguage || window.navigator.language).substring(0,2);
+const userLocale = languageMappings[userLanguage] ? userLanguage : 'en';
+
 const i18n = new VueI18n({
-  locale: 'en',
-  messages: {
-    'en': require('./en.json'),
-    'ja': require('./ja/en.json'),
-    'fr': require('./fr/en.json'),
-    'es': require('./es/en.json'),
-    'ar': require('./ar/en.json'),
-    'zh': require('./zh/en.json'),
-  }
+  locale: userLocale,
+  messages: languageMappings,
 })
 
 window.i18n = i18n;
-
-if (module.hot) {
-  module.hot.accept(['./en.json', './ja/en.json'], () => {
-    i18n.setLocaleMessage('en', require('./en.json'))
-    i18n.setLocaleMessage('ja', require('./ja/en.json'))
-    console.log('hot reload', this, arguments)
-  })
-}
 
 export default i18n
